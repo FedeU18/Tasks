fd2186519abfb6ba1b764ba752a9a9391f5f84cc
Federico <fedee.unates.2001@gmail.com>
2024-03-01 02:37:30 -0300

    validaciones al crear y actualizar una task

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,22 @@
 import { TaskStatus } from '../task.entity';
+import { IsIn, IsOptional, IsString } from 'class-validator';
 
 export class CreateTaskDTO {
+  @IsString()
   title: string;
+  @IsString()
   description: string;
 }
 
 export class UpdateTaskDTO {
+  @IsString()
+  @IsOptional()
   title?: string;
+  @IsString()
+  @IsOptional()
   description?: string;
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.PENDING])
   status?: TaskStatus;
 }
